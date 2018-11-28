28ff2099a25325583db1a5c2e77bd2fccbfa57e2
Rorshachk <shuangmuhu@gmail.com>
2018-11-28 23:46:22 +0800

    添加外星人以及子弹计数

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,7 @@
 import pygame
 import game_functions as gf
+from alien import Alien
+from ship import Ship
 from pygame.sprite import Group
 
 
@@ -14,37 +16,7 @@ class Settings():  # 设置类
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-
-
-class Ship():
-    def __init__(self, ai_settings, screen):
-        self.screen = screen
-        self.ai_settings = ai_settings
-        self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
-        # print(self.rect)
-        self.screen_rect = screen.get_rect()
-        # print(self.screen_rect)
-
-        # 把飞船放到屏幕下方的中间
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
-
-        self.center = float(self.rect.centerx)
-
-        self.moving_right = False
-        self.moving_left = False
-
-    def update(self):
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
-        # 更新飞船的位置
-        self.rect.centerx = self.center
-
-    def blitme(self):
-        self.screen.blit(self.image, self.rect)
+        self.bullet_allowed = 3
 
 
 def run_game():
@@ -58,16 +30,18 @@ def run_game():
     ship = Ship(ai_settings, screen)
 
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    alien = Alien(ai_settings, screen)
 
     # 开始游戏
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
-        for bullet in bullets.copy():
-        	if bullet.rect.bottom <= 0:
-        		bullets.remove(bullet)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -1,7 +1,15 @@
 import pygame
 import sys
 from bullet import Bullet
+from alien import Alien
 from pygame.sprite import Group
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
@@ -9,8 +17,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -30,10 +37,56 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def get_number_alien_x(ai_settings, alien_width):
+    alien_width = alien_width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def get_number_alien_y(ai_settings, ship_height, alien_height):
+    available_space_y = (ai_settings.screen_height -
+                         (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+
+    alien.x = alien_width + 2 * alien_number * alien_width
+    alien.rect.x = alien.x
+
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+
+    aliens.add(alien)
+
+
+def create_fleet(ai_settings, screen, ship, aliens):
+    alien = Alien(ai_settings, screen)
+
+    number_alien_x = get_number_alien_x(ai_settings, alien.rect.width)
+    number_rows = get_number_alien_y(
+        ai_settings, ship.rect.height, alien.rect.height)
+
+    for row_number in range(number_rows):
+        for alien_number in range(number_alien_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    aliens.draw(screen)
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
