bea33c63cee4d9024ceff6b234031fd00422ae3e
Rorshachk <shuangmuhu@gmail.com>
2018-12-06 23:49:06 +0800

    摸鱼

--- Data visualization/die_visual.py
+++ Data visualization/die_visual.py
@@ -2,22 +2,24 @@ from die import Die
 import pygal
 
 
-die = Die()
+die1 = Die()
+die2 = Die()
 results = []
 for roll_num in range(1000):
-    results.append(die.roll())
+    results.append(die1.roll() + die2.roll())
 
 frequencies = []
-for value in range(1, die.num_sizes + 1):
+max_result = die1.num_sizes + die2.num_sizes
+for value in range(2, max_result + 1):
     frequencies.append(results.count(value))
 print(frequencies)
 
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
+hist.add('D6 + D6', frequencies)
 hist.render_to_file('die_visual.svg')
