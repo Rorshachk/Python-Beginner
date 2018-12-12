ebd53000c59e7f4cde5299674e4c19f95d2266fa
Rorshachk <shuangmuhu@gmail.com>
2018-12-12 23:20:18 +0800

    重做..

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wv)j!hbyl%u)g7p%=q!)9i66s*pwfzh)4$z*(n07njl&jz&%k-'
+SECRET_KEY = 'o(kvifm9jjo4!63h=4-3eoub=kr(xd@dt$aw^&_#d&w(psgztg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,6 +1,3 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
 from django.db import models
 
 # Create your models here.
@@ -8,15 +5,14 @@ from django.db import models
 
 class Topic(models.Model):
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
 
 
 class Entry(models.Model):
-
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
