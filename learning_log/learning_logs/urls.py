4834bab95b36bc65a97a5407cc2e56ab7f862681
Rorshachk <shuangmuhu@gmail.com>
2018-12-14 23:48:45 +0800

    。。。

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -8,5 +8,7 @@ from . import views
 urlpatterns = [
     # 主页
     url(r'^$', views.index, name='index'),
+    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
 app_name = "learning_logs"
