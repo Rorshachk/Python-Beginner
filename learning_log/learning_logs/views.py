62a4530a1399d324bce9a79ccf4be6ebecc11c39
Rorshachk <shuangmuhu@gmail.com>
2018-12-19 23:46:14 +0800

    19.31 19.32

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 from .models import Topic, Entry
@@ -12,6 +13,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
