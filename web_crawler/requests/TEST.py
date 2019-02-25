9a884bdc21ebf859846d2217d4a60d5168604453
Rorshachk <shuangmuhu@gmail.com>
2019-02-25 23:18:28 +0800

    water

--- web_crawler/requests/TEST.py
+++ web_crawler/requests/TEST.py
@@ -1,4 +1,5 @@
 import requests as rq
+import re
 
 r = rq.get('http://www.baidu.com')
 print(type(r))
@@ -21,3 +22,16 @@ r = rq.get('http://httpbin.org/get')
 print(type(r.text))
 print(r.json())
 print(type(r.json()))
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+r = rq.get("https://www.zhihu.com/explore", headers=headers)
+print(r.text)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
+
+r = rq.get('https://github.com/favicon.ico')
+with open('favicon.ico', 'wb') as f_obj:
+    f_obj.write(r.content)
